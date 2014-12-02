3013d3081aa1ef1065b8e9843c290c67718479ad
Dan Sim <dsim@au.westfield.com>
2014-12-02 15:55:33 -0800

    Hack beacon demo into Westfield touch to phone demo

--- closest-beacon-demo/ViewController.swift
+++ closest-beacon-demo/ViewController.swift
@@ -10,37 +10,83 @@ import UIKit
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-    
-    let locationManager = CLLocationManager()
-    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D"), identifier: "Estimotes")
-    let colors = [
-        54482: UIColor(red: 84/255, green: 77/255, blue: 160/255, alpha: 1),
-        31351: UIColor(red: 142/255, green: 212/255, blue: 220/255, alpha: 1),
-        27327: UIColor(red: 162/255, green: 213/255, blue: 181/255, alpha: 1)
-    ]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        locationManager.delegate = self
-        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse) {
-            locationManager.requestWhenInUseAuthorization()
-        }
-        locationManager.startRangingBeaconsInRegion(region)
-    }
+  let locationManager = CLLocationManager()
+  let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "23538C90-4E4C-4183-A32B-381CFD11C465"), identifier: "Nomi")
+  var bytes: NSMutableData?
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var descriptionLabel: UITextView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    locationManager.delegate = self
+    if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse) {
+      locationManager.requestWhenInUseAuthorization()
     }
-    
-    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
-        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
-        if (knownBeacons.count > 0) {
-            let closestBeacon = knownBeacons[0] as CLBeacon
-            self.view.backgroundColor = self.colors[closestBeacon.minor.integerValue]
-        }
+    locationManager.startRangingBeaconsInRegion(region)
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
+    let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
+    if (knownBeacons.count > 0) {
+      let closestBeacon = knownBeacons[0] as CLBeacon
+
+      var message:String = ""
+
+      switch closestBeacon.proximity {
+      case CLProximity.Far:
+        message = "You are far away from the beacon"
+      case CLProximity.Near:
+        message = "You are near the beacon"
+      case CLProximity.Immediate:
+        message = "You're on it. Retrieving JSON state"
+        NSLog(message)
+        //Retrieve State of Kiosk
+        let url = "http://kiosk-service-proto.herokuapp.com/api/kiosks/master/kiosks/23/state"
+        let request = NSURLRequest(URL: NSURL(string: url)!)
+        let loader = NSURLConnection(request: request, delegate: self, startImmediately: true)
+      case CLProximity.Unknown:
+        return
+      }
+
     }
-    
+  }
+
+
+  func connection(connection: NSURLConnection!, didReceiveData conData: NSData!) {
+    self.bytes?.appendData(conData)
+  }
+
+  func connection(didReceiveResponse: NSURLConnection!, didReceiveResponse response: NSURLResponse!) {
+    self.bytes = NSMutableData()
+  }
+
+  func connectionDidFinishLoading(connection: NSURLConnection!) {
+
+    // we serialize our bytes back to the original JSON structure
+    let jsonResult: Dictionary = NSJSONSerialization.JSONObjectWithData(self.bytes!, options: NSJSONReadingOptions.MutableContainers, error: nil) as Dictionary<String, AnyObject>
+    let destination:Dictionary = jsonResult["destination"] as Dictionary<String, AnyObject>
+
+    let storeName:String = destination["name"] as String
+    let storeDescription:String = destination["description"] as String
+
+    self.titleLabel.text = storeName
+    self.descriptionLabel.text = storeDescription
+
+    NSLog("Find and show store: \(storeName), \(storeDescription)")
+
+
+
+  }
+
+
+
 }
 
